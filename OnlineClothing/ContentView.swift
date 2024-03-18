ebe8e46500fce8473ef56186f54da82c166c7ff1
AvithashFernando <avithashfdo99@gmail.com>
2024-03-18 11:37:04 +0530

    Initial Commit

---
+++ OnlineClothing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnlineClothing
+//
+//  Created by NIBM-LAB04-PC04 on 2024-03-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnlineClothing/OnlineClothingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineClothingApp.swift
+//  OnlineClothing
+//
+//  Created by NIBM-LAB04-PC04 on 2024-03-18.
+//
+
+import SwiftUI
+
+@main
+struct OnlineClothingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
